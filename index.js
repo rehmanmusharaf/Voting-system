371d7b671460b5b41be8ae88b6168e4a3faaf583
Muhammad Rehman <124819760+rehmanmusharaf@users.noreply.github.com>
2024-05-02 13:26:10 +0500

    changed index.html
    chan

--- index.js
+++ index.js
@@ -59,7 +59,5 @@ app.listen(port, () => {
 app.use(express.static(path.join(__dirname, "/Voting-System-main/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/Voting-System-main", "build", "index.html"));
-  console.log("Resolved file path:", filePath);
-  res.sendFile(filePath);
+  res.sendFile(path.join(__dirname, "Voting-System-main", "build", "index.html"));
 });

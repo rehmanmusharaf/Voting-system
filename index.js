6a196e9471a5dea35f42090079fb3df6de90e03b
Muhammad Rehman <muhammadrehmanmusharafali@gmail.com>
2024-05-04 00:08:52 +0500

    build folder added

--- index.js
+++ index.js
@@ -56,10 +56,12 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.use(express.static(path.join(__dirname, "/Voting-System-main/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  res.sendFile(
+    path.join(__dirname, "Voting-System-main", "build", "index.html")
+  );
   console.log("Resolved file path:", filePath);
   res.sendFile(filePath);
 });

2cd1c31a178161b6e3716f2baaae5f5462fd0211
Muhammad Rehman <muhammadrehmanmusharafali@gmail.com>
2024-05-04 19:46:07 +0500

    PORT NUMBER ADDED

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8080;
 const symbol = require("./Controlers/symbol.js");
 const vote = require("./Controlers/vote.js");
 const election = require("./Controlers/election.js");
